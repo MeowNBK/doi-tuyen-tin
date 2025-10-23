9436d6e8406d24a600f2223dc0ae3d97c6d8033c
MeowNBK <dragon.lostsky@gmail.com>
2025-10-23 15:09:15 +0700

    Add files via upload

---
+++ bai_tap_1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define EPSILON 1e-9
+#define MEOW_PI 3.141592654
+
+template <typename...Args>
+inline void print(Args&&...args) noexcept {
+    (cout << ... << args);
+}
+
+template <typename...Args>
+inline void printl(Args&&...args) noexcept {
+    (cout << ... << args);
+    cout << endl;
+}
+
+inline double to_radian(double x) noexcept {
+    return (x * 180.0)/MEOW_PI;
+}
+
+inline double to_deg(double x) noexcept {
+    return (x * 180.0)/MEOW_PI;
+}
+
+inline array<double, 3> calculate(double a, double b, double c) noexcept {
+    double A, B, C;
+
+    double cosA = (b*b + c*c - a*a) / (2*b*c);
+    double cosB = (a*a + c*c - b*b) / (2*a*c);
+
+    A = acos(cosA);
+    B = acos(cosB);
+
+    A = to_deg(A);
+    B = to_deg(B);
+
+    C = 180 - A - B;
+
+    return array<double, 3>{ A, B, C };
+}
+
+int main() {
+    double a, b, c;
+    double A, B, C;
+    cin >> a >> b >> c;
+
+    // Tim xem day la tam giac gi: thuong, vuong, can
+
+    double canh_lon_nhat = max(max(a, b), c);
+
+    auto result = calculate(a, b, c);
+    A = result[0];
+    B = result[1];
+    C = result[2];
+
+    // printl(a, ' ', b, ' ', c);
+    // printl(A, ' ', B, ' ', C);
+    return 0;
+}
